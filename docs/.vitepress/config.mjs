f08720fef0ae1e16cde3984ebfad7ed65f57f20e
fast1233e <133764560+fast1233e@users.noreply.github.com>
2023-09-01 22:56:03 +0800

    update

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -8,19 +8,136 @@ export default defineConfig({
     themeConfig: {
         // https://vitepress.dev/reference/default-theme-config
         nav: [
-            { text: 'Home', link: '/' },
-            { text: 'Examples', link: '/markdown-examples' }
+            { text: '首页', link: '/' },
+            { text: '基础', link: '/basis/css' },
+            { text: '进阶', link: '/advanced/es6' },
+            { text: '实现', link: '/achieve/index' },
+            { text: 'Vue', link: '/vue/index' },
+            { text: 'React', link: '/react/index' },
+            { text: 'Plugin', link: '/plugin/vue' },
+            { text: 'Skill', link: '/skill/notes' },
         ],
 
-        sidebar: [
-            {
-                text: 'Examples',
+        sidebar: {
+            '/basis/': {
+                base: '/basis/',
                 items: [
-                    { text: 'Markdown Examples', link: '/markdown-examples' },
-                    { text: 'Runtime API Examples', link: '/api-examples' }
+                    {
+                        text: 'CSS',
+                        collapsed: false,
+                        items: [
+                            { text: 'CSS', link: '/css' },
+                            { text: 'HTML', link: '/html' },
+                            { text: 'javascript', link: '/javascript' },
+                        ]
+                    },
+                    {
+                        text: 'HTML',
+                        collapsed: false,
+                        items: [
+                            { text: 'CSS', link: '/css' },
+                            { text: 'HTML', link: '/html' },
+                            { text: 'javascript', link: '/javascript' },
+                        ]
+                    },
+                    {
+                        text: 'javascript',
+                        collapsed: false,
+                        items: [
+                            { text: 'CSS', link: '/css' },
+                            { text: 'HTML', link: '/html' },
+                            { text: 'javascript', link: '/javascript' },
+                        ]
+                    },
                 ]
-            }
-        ],
+            },
+            '/advanced/': {
+                base: '/advanced/',
+                items: [
+                    {
+                        text: '进阶',
+                        collapsed: false,
+                        items: [
+                            { text: 'ES6', link: '/es6' },
+                        ]
+                    },
+                ]
+            },
+            '/achieve/': {
+                base: '/achieve/',
+                items: [
+                    {
+                        text: '实现',
+                        collapsed: false,
+                        items: [
+                            { text: '介绍', link: '/index' },
+                            { text: 'promise', link: '/promise' },
+                        ]
+                    },
+                ]
+            },
+            '/vue/': {
+                base: '/vue/',
+                items: [
+                    {
+                        text: 'vue',
+                        collapsed: false,
+                        items: [
+                            { text: '介绍', link: '/index' },
+                        ]
+                    },
+                ]
+            },
+            '/react/': {
+                base: '/react/',
+                items: [
+                    {
+                        text: 'react',
+                        collapsed: false,
+                        items: [
+                            { text: '介绍', link: '/index' },
+                        ]
+                    },
+                ]
+            },
+            '/plugin/': {
+                base: '/plugin/',
+                items: [
+                    {
+                        text: 'plugin',
+                        collapsed: false,
+                        link: '/vue'
+                    },
+                ]
+            },
+            '/skill/': {
+                base: '/skill/',
+                text: 'skill',
+                items: [
+                    {
+                        text: '简历准备',
+                        link: '/notes'
+                    },
+                    {
+                        text: '面试前的准备',
+                        link: '/preparation'
+
+                    },
+                    {
+                        text: '面试的注意事项',
+                        link: '/matter'
+                    },
+                    {
+                        text: '如何通过HR⾯',
+                        link: '/hr'
+                    },
+                    {
+                        text: '面试后的思考总结',
+                        link: '/summarize'
+                    },
+                ]
+            },
+        },
 
         socialLinks: [
             { icon: 'github', link: 'https://github.com/weilkss' }
--- docs/index.md
+++ docs/index.md
@@ -3,26 +3,45 @@
 layout: home
 
 hero:
-  name: "blog"
-  text: "weilkss blog"
+  name: "Weilkss"
+  text: "学习和知识储备"
   tagline: 真正的大师永远都怀着一颗学徒的心。
   actions:
     - theme: brand
       text: 基础
-      link: /markdown-examples
+      link: /basis/css
     - theme: brand
-      text: 升级
-      link: /api-examples
+      text: 进阶
+      link: /advanced
+    - theme: brand 
+      text: 代码实现
+      link: /achieve
     - theme: alt
-      text: Vue & React
-      link: /api-examples
+      text: Vue
+      link: /achieve
+    - theme: alt
+      text: React
+      link: /achieve
       
 features:
-  - title: Feature A
-    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
-  - title: Feature B
-    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
-  - title: Feature C
-    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+  - title: 基础知识
+    details: 作为一个程序员，基础知识必须扎实，要做到即问即答，不拖泥带水
+  - title: 技术进阶
+    details: 要掌握一些工作中常用的技能，让自己的技术能够得到扩展
+  - title: 手写实现
+    details: 实现一些新的API和方法，使得更容易理解是使用
+  - title: 技术栈
+    details: 对Vue和React进行源码分析，一步一步实现mini版本
+  - title: 收藏轮子
+    details: 对工作中常使用的一些插件和方法进行整理
+  - title: 面试技巧
+    details: 记录和整理一些面试技巧，让找工作变得很美好
 ---
 
+<style lang='scss'>
+    .container{
+        .text{
+            margin-top:20px
+        }
+    }
+</style>
